3062447fe35c5b4aeb77bae50680ba0a2be2e04a
Hoy-Code <maurorafael09@gmail.com>
2022-06-07 17:26:04 +0100

    commit

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,10 +1,10 @@
 // Hide menu on scroll down
 // Show menu on scroll up
 
-let didScroll;
-let lastScrollTop = 0;
-let delta = 5;
-let navbarHeight = $('.container-header').outerHeight();
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('.container-header').outerHeight();
 
 $(window).scroll(function () {
     didScroll = true;
@@ -18,7 +18,7 @@ setInterval(function () {
 }, 250);
 
 function hasScrolled() {
-    let st = $(this).scrollTop();
+    var st = $(this).scrollTop();
 
     // Make sure they scroll more than delta
     if (Math.abs(lastScrollTop - st) <= delta)
@@ -41,22 +41,24 @@ function hasScrolled() {
     lastScrollTop = st;
 }
 
-$(document).ready(() => {
+//When the page is loaded, execute all the code (functions) inside
+
+$(document).ready(function () {
 
     //Show or hide bag menu on bag-icon click (menu)
 
-    $('#bag-icon').click(() => {
+    $('#bag-icon').click(function () {
         $('.bag-container').toggleClass('hide');
     })
 
-    //Seach Icon click
+    //Shows the search-bar on the search-icon click (menu)
 
     $('#search-icon').click(function () {
         menuToggle();
         searchShow();
         $('.bag-container').addClass('hide');
 
-        let count = 0;
+        var count = 0;
 
         $(window).resize(function () {
             if ($(this).width() < 850 && count == 0) {
@@ -78,7 +80,7 @@ $(document).ready(() => {
 
     })
 
-    $('.overlay').click(() => {
+    $('.overlay').click(function () {
         menuToggle();
         searchHide();
 
@@ -100,19 +102,19 @@ $(document).ready(() => {
 
 //Functions
 
-const menuToggle = () => {
+const menuToggle = function () {
     $('.menu-item').toggleClass('hide');
     $('.menu-icons').toggleClass('hide')
 }
 
-const searchHide = () => {
+const searchHide = function () {
     $('.search-container').addClass('hide');
     $('.overlay').addClass('hide');
     $('body').removeClass('hide-scrollbar');
     $('.container-header').removeClass('bgc');
 }
 
-const searchShow = () => {
+const searchShow = function () {
     $('.search-container').removeClass('hide');
     $('.overlay').removeClass('hide');
     $('body').addClass('hide-scrollbar');
@@ -148,115 +150,188 @@ function closeNavCountry() {
 
 //Populate sidenav bar menu using a json list
 
-let sidenavDynamic = () => {
-    $.getJSON("./menujson.json", function (data) {
+var sidenavDynamic = function () {
+    $.getJSON("../data/menujson.json", function (data) {
 
-        //console.log(data);
+        // var array = [];
+        // for (var i = 0; i < data.length; i++) {
+        //     array.push(data[i].opt);
+        // }
 
-        let array = [];
-        for (let i = 0; i < data.length; i++) {
-            array.push(data[i].opt);
-        }
+        // console.log(array);
 
-        console.log(array);
+        // var list = document.createElement("ul");
+        // for (var i of array) {
+        // var listItem = document.createElement("li");
+        // var aItem = document.createElement("a");
+        // aItem.innerHTML = i;
+        // listItem.appendChild(aItem);
+        // list.appendChild(listItem);
+        // }
 
-        // (B2) CREATE LIST
+        // $("#sidenav-options").append(list);
+
         var list = document.createElement("ul");
-        for (let i of array) {
-            let listItem = document.createElement("li");
-            let aItem = document.createElement("a");
-            aItem.innerHTML = i;
-            listItem.appendChild(aItem);
-            list.appendChild(listItem);
+
+        var myResult = data;
+
+        for (var i in myResult) {
+            if (myResult[i] instanceof Object) {
+
+                //console.log(myResult[i]);
+
+                $.each(myResult[i], function (key, value) {
+                    //console.log(key + ": " + value);
+                    if (key == "opt") {
+                        var listItem = document.createElement("li");
+                        var aItem = document.createElement("a");
+                        aItem.innerHTML = value;
+                        listItem.appendChild(aItem);
+                        list.appendChild(listItem);
+                    }
+                });
+            }
         }
 
-        // (B3) APPEND LIST TO CONTAINER
         $("#sidenav-options").append(list);
 
     })
 }
 
-//Populate nav bar menu using a json list
+//Populate nav bar menu using a json list - AINDA NÃO FUNCIONA
 
-let navDynamic = () => {
-    $.getJSON("./menujson.json", function (data) {
+var navDynamic = function () {
+    $.getJSON("../data/menujson.json", function (data) {
 
         //console.log(data);
 
-        let array = [];
-        for (let i = 0; i < data.length; i++) {
+        var array = [];
+        for (var i = 0; i < data.length; i++) {
             array.push(data[i].opt);
         }
 
         console.log(array);
 
-        // (B2) CREATE LIST
-
         var list = document.createElement("ul");
+
         $(list).css('list-style', 'none');
-        for (let i of array) {
-            let listItem = document.createElement("li");
-            let aItem = document.createElement("a");
+        for (var i of array) {
+            var listItem = document.createElement("li");
+            var aItem = document.createElement("a");
             aItem.classList.add('menu-item');
             aItem.innerHTML = i;
             listItem.appendChild(aItem);
             list.appendChild(listItem);
         }
 
-        // (B3) APPEND LIST TO CONTAINER
         $("#menu-options").append(list);
-
     })
 
 }
 
 //Populate countries menu using a json list
 
-let countryDynamic = () => {
-    $.getJSON("./eucountries.json", function (data) {
+var countryDynamic = function () {
+    $.getJSON("../data/eucountries.json", function (data) {
 
         //console.log(data);
 
-        let arrayCountries = [];
-        for (let i = 0; i < data.length; i++) {
+        var arrayCountries = [];
+        for (var i = 0; i < data.length; i++) {
 
             arrayCountries.push([data[i].name, data[i].language]);
         }
 
-        console.log(arrayCountries);
+        var createList = function (i, arrayCountries, list) {
 
-        // (B2) CREATE LIST
-        var list = document.createElement("ul");
+            var listItem = document.createElement("li");
 
-        for (let i = 0; i < arrayCountries.length; i++) {
-            let listItem = document.createElement("li");
-            
-                    let aItem = document.createElement("a");
-                    let pItem = document.createElement("p");
-                    // let sItem = document.createElement("span");
+            var aItem = document.createElement("a");
+            var pItem = document.createElement("p");
+            // var sItem = document.createElement("span");
 
-                    aItem.innerHTML = arrayCountries[i][0];
-                    pItem.innerHTML = arrayCountries[i][1];
+            aItem.innerHTML = arrayCountries[i][0];
+            pItem.innerHTML = arrayCountries[i][1];
 
-                    //aItem.classList.add('country');
-                    aItem.setAttribute('href', data[i].href);
-                    // sItem.classList.add('fi');
-                    // sItem.classList.add('fi-gr');
+            //aItem.classList.add('country');
+            aItem.setAttribute('href', data[i].href);
+            // sItem.classList.add('fi');
+            // sItem.classList.add('fi-gr');
 
+            listItem.appendChild(aItem);
+            listItem.appendChild(pItem);
+            // listItem.appendChild(sItem);
+            list.appendChild(listItem);
 
-                    listItem.appendChild(aItem);
-                    listItem.appendChild(pItem);
-                    // listItem.appendChild(sItem);
-                    list.appendChild(listItem);
-
-            
         }
 
-        // (B3) APPEND LIST TO DIV
-        $('.countries-list').append(list);
+        console.log(arrayCountries);
+
+        var listCountryOne = document.createElement("ul");
+        for (var i = 0; i < 11; i++) {
+            createList(i, arrayCountries, listCountryOne);
+        }
+
+        var listCountryTwo = document.createElement("ul");
+        for (var i = 11; i < 22; i++) {
+            createList(i, arrayCountries, listCountryTwo);
+        }
+
+        var listCountryThree = document.createElement("ul");
+        for (var i = 22; i < 33; i++) {
+            createList(i, arrayCountries, listCountryThree);
+        }
+
+        var listCountryFour = document.createElement("ul");
+        for (var i = 33; i < 43; i++) {
+            createList(i, arrayCountries, listCountryFour);
+        }
+
+        $('.countries-list').append(listCountryOne);
+        $('.countries-list').append(listCountryTwo);
+        $('.countries-list').append(listCountryThree);
+        $('.countries-list').append(listCountryFour);
+
+
 
     })
 }
+
+// const bagDynamicList = () => {
+
+//     $.getJSON("../data/bag.json", function (data) {
+//         var arrayBagOptions = [];
+
+//         for (var i = 0; i < data.length; i++) {
+
+//             arrayBagOptions.push(data[i].option);
+//         }
+//         console.log(arrayBagOptions);
+
+//         var list = document.createElement("ul");
+
+//         for (var i = 0; i < arrayBagOptions.length; i++) {
+
+//             var listItem = document.createElement("li");
+//             var aItem = document.createElement("a");
+
+//             switch (i) {
+//                 case 0:
+//                     $('bag-options a').attr('id', 'menu-bag-icon');
+//                     break;
+//                 case 1:
+//                     $('bag-options a').attr('id', 'menu-saved-icon');
+//                     break;
+//             }
+
+//             listItem.appendChild(aItem);
+//             list.appendChild(listItem);
+//         }
+
+//         $(".bag-options").append(list);
+//     })
+
+// }
 
 
 //Get country by IP
@@ -280,7 +355,7 @@ $.getJSON("https://api64.ipify.org?format=json", function (json) {
 
     var ip = json.ip;
     var api_key = "at_474dIdFdbKouxBRhIED0n3meavWIl";
-    //let ptCount = 0;
+    //var ptCount = 0;
     $(function () {
         $.ajax({
             url: "https://geo.ipify.org/api/v2/country?apiKey=at_474dIdFdbKouxBRhIED0n3meavWIl",
@@ -289,12 +364,12 @@ $.getJSON("https://api64.ipify.org?format=json", function (json) {
                 ipAddress: ip
             },
             success: function (data) {
-                $.getJSON("./countries.json", function (countries) {
-                    for (let i = 0; i < countries.length; i++) {
+                $.getJSON("../data/countries.json", function (countries) {
+                    for (var i = 0; i < countries.length; i++) {
                         if (countries[i].code === data.location.country) {
                             $("#ip-text").append(countries[i].name);
                             // ptCount ++;
-                            // let ipTest = document.createElement("p");
+                            // var ipTest = document.createElement("p");
                             // ipTest.innerHTML = ptCount;
                             //document.getElementById("ptCount").appendChild(ipTest);
                         }
